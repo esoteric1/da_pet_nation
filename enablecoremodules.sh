17ac683dc243d33d02ee6f5acf04ed4b5c8fe867
Marc Andre Rousseau <mrousseaumtl@gmail.com>
2013-02-13 12:23:06 -0500

    removed script

--- enablecoremodules.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-# Enables core modules that weren't enabled in our profile installation
-
-drush en block color comment contextual dashboard help image list menu number options path taxonomy dblog search shortcut overlay field_ui file rdf -y
-
