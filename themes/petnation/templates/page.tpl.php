a86eab63098011e9b464e7fd94c5919e38a1376f
Tina Holly (Tinacious Design) <tinacious.design@gmail.com>
2013-03-02 19:02:17 -0500

    css files modified by compass to remove comments, zen grid & responsive design example implemented

--- themes/petnation/templates/page.tpl.php
+++ themes/petnation/templates/page.tpl.php
@@ -71,112 +71,99 @@
 ?>
 
 <header id="header" role="banner">
-  <div class="container">
 
-    <a href="<?php print $front_page;?>" title="<?php print t('Home');?>" rel="home">
-      <img src="<?php print path_to_theme();?>/images/petnation-logo.png"/>
-    </a>
+	<div class="container">
+		<div class="header-content">
+			<a href="<?php print $front_page;?>" title="<?php print t('Home');?>" rel="home">
+				<img src="<?php print path_to_theme();?>/images/petnation-logo.png"/>
+			</a>
 
-    <?php if ($site_name || $site_slogan): ?>
-      <hgroup id="name-and-slogan">
-        <?php if ($site_name): ?>
-          <h1 id="site-name">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-          </h1>
-        <?php endif; ?>
+			<?php if ($secondary_menu): ?>
+				<nav id="secondary-menu" role="navigation">
+					<?php print theme('links__system_secondary_menu', array(
+						'links' => $secondary_menu,
+						'attributes' => array(
+							'class' => array('links', 'inline', 'clearfix'),
+						),
+						'heading' => array(
+							'text' => $secondary_menu_heading,
+							'level' => 'h2',
+							'class' => array('element-invisible'),
+						),
+					)); ?>
+				</nav>
+			<?php endif; ?>
 
-        <?php if ($site_slogan): ?>
-          <h2 id="site-slogan"><?php print $site_slogan; ?></h2>
-        <?php endif; ?>
-      </hgroup><!-- /#name-and-slogan -->
-    <?php endif; ?>
+		</div>
+	</div><?php //.container ?>
 
-    <?php if ($secondary_menu): ?>
-      <nav id="secondary-menu" role="navigation">
-        <?php print theme('links__system_secondary_menu', array(
-          'links' => $secondary_menu,
-          'attributes' => array(
-            'class' => array('links', 'inline', 'clearfix'),
-          ),
-          'heading' => array(
-            'text' => $secondary_menu_heading,
-            'level' => 'h2',
-            'class' => array('element-invisible'),
-          ),
-        )); ?>
-      </nav>
-    <?php endif; ?>
+</header>
 
-    <?php print render($page['header']); ?>
+	<div id="main">
+		<div class="container">
 
-    </div>
-  </header>
+		<div id="content" class="column" role="main">
+			<?php print render($page['highlighted']); ?>
+			<?php print $breadcrumb; ?>
+			<a id="main-content"></a>
+			<?php print render($title_prefix); ?>
+			<?php if ($title): ?>
+				<h1 class="title" id="page-title"><?php print $title; ?></h1>
+			<?php endif; ?>
+			<?php print render($title_suffix); ?>
+			<?php print $messages; ?>
+			<?php print render($tabs); ?>
+			<?php print render($page['help']); ?>
+			<?php if ($action_links): ?>
+				<ul class="action-links"><?php print render($action_links); ?></ul>
+			<?php endif; ?>
+			<?php print render($page['content']); ?>
+			<?php print $feed_icons; ?>
+		</div><!-- /#content -->
 
-  <div id="main">
-    <div class="container">
+		<div id="navigation">
 
-    <div id="content" class="column" role="main">
-      <?php print render($page['highlighted']); ?>
-      <?php print $breadcrumb; ?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="title" id="page-title"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php print render($tabs); ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
-    </div><!-- /#content -->
+			<?php if ($main_menu): ?>
+				<nav id="main-menu" role="navigation">
+					<?php
+					// This code snippet is hard to modify. We recommend turning off the
+					// "Main menu" on your sub-theme's settings form, deleting this PHP
+					// code block, and, instead, using the "Menu block" module.
+					// @see http://drupal.org/project/menu_block
+					print theme('links__system_main_menu', array(
+						'links' => $main_menu,
+						'attributes' => array(
+							'class' => array('links', 'inline', 'clearfix'),
+						),
+						'heading' => array(
+							'text' => t('Main menu'),
+							'level' => 'h2',
+							'class' => array('element-invisible'),
+						),
+					)); ?>
+				</nav>
+			<?php endif; ?>
 
-    <div id="navigation">
+			<?php print render($page['navigation']); ?>
 
-      <?php if ($main_menu): ?>
-        <nav id="main-menu" role="navigation">
-          <?php
-          // This code snippet is hard to modify. We recommend turning off the
-          // "Main menu" on your sub-theme's settings form, deleting this PHP
-          // code block, and, instead, using the "Menu block" module.
-          // @see http://drupal.org/project/menu_block
-          print theme('links__system_main_menu', array(
-            'links' => $main_menu,
-            'attributes' => array(
-              'class' => array('links', 'inline', 'clearfix'),
-            ),
-            'heading' => array(
-              'text' => t('Main menu'),
-              'level' => 'h2',
-              'class' => array('element-invisible'),
-            ),
-          )); ?>
-        </nav>
-      <?php endif; ?>
+		</div><!-- /#navigation -->
 
-      <?php print render($page['navigation']); ?>
+		<?php
+			// Render the sidebars to see if there's anything in them.
+			$sidebar_first  = render($page['sidebar_first']);
+			$sidebar_second = render($page['sidebar_second']);
+		?>
 
-    </div><!-- /#navigation -->
+		<?php if ($sidebar_first || $sidebar_second): ?>
+			<aside class="sidebars">
+				<?php print $sidebar_first; ?>
+				<?php print $sidebar_second; ?>
+			</aside><!-- /.sidebars -->
+		<?php endif; ?>
 
-    <?php
-      // Render the sidebars to see if there's anything in them.
-      $sidebar_first  = render($page['sidebar_first']);
-      $sidebar_second = render($page['sidebar_second']);
-    ?>
+		</div> <!--/.container -->
+	</div><!-- /#main -->
 
-    <?php if ($sidebar_first || $sidebar_second): ?>
-      <aside class="sidebars">
-        <?php print $sidebar_first; ?>
-        <?php print $sidebar_second; ?>
-      </aside><!-- /.sidebars -->
-    <?php endif; ?>
-
-    </div> <!--/.container -->
-  </div><!-- /#main -->
-
-  <?php print render($page['footer']); ?>
+	<?php print render($page['footer']); ?>
 
 <?php print render($page['bottom']); ?>
